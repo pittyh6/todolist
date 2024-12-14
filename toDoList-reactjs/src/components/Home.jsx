6f84140508ad2ff3c2665ea5d9babb11125add83
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-12-14 16:04:40 +1100

    fix app and home page to show components correctly

--- toDoList-reactjs/src/components/Home.jsx
+++ toDoList-reactjs/src/components/Home.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Dropbox from "./Dropbox";
 import AddTask, { tasksArr } from "./AddTask";
+import Dropbox from "./Dropbox";
 import Tasks from "./Tasks";
 
 function Home(props) {
-  const displayTasksArr = tasksArr.map((taskElement) => <p>{taskElement}</p>);
-  console.log("array from addTask: " + displayTasksArr);
-
   return (
     <div>
       <h1>To Do List</h1>
       <div className="addTask">
-        {/* <input type="text" placeholder="Insert your tasks here" />
-        <button>+</button> */}
         <AddTask />
       </div>
       <div className="filtering">
