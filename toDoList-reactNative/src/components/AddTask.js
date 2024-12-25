5413714fddca201a337a924c1f6d749a058d3752
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-12-25 14:28:12 +1100

    start addTask

--- toDoList-reactNative/src/components/AddTask.js
+++ toDoList-reactNative/src/components/AddTask.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 
+
+
+function handleAddTask(e) {
+    console.log('addTask button')
+}
 function AddTask(props) {
+    const [addTaskText, setAddTaskText] = useState("")
     return (
         <View>
             <TextInput
                 placeholder="Insert your task"
             />
+            <TouchableHighlight >
+                <Text onPress={handleAddTask}>+</Text>
+            </TouchableHighlight>
         </View>
     );
 }
