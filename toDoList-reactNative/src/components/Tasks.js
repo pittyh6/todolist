054316343660d20a8b706797f73eddac39f5e23e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-01-03 08:00:16 +1100

    function completeTask and DeleteTask.

--- toDoList-reactNative/src/components/Tasks.js
+++ toDoList-reactNative/src/components/Tasks.js
@@ -1,19 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
-    StyleSheet, Text, View, SafeAreaView, Animated,
-    PanResponder, TouchableOpacity, FlatList,
+    StyleSheet, Text, View, SafeAreaView, Animated, TouchableOpacity, FlatList,
 } from 'react-native';
-
+import { Swipeable } from 'react-native-gesture-handler';
 
 
 function Tasks(props) {
-    const tasks = [
+    const [tasks, setTasks] = useState([
         { id: 1, taskDescription: "React Native", taskStatus: "Pending" },
         { id: 2, taskDescription: "Read", taskStatus: "Completed" },
-        { id: 3, taskDescription: "Draw", taskStatus: "Completed" },
+        { id: 3, taskDescription: "Draw", taskStatus: "Pending" },
         { id: 4, taskDescription: "Rest", taskStatus: "Completed" },
-    ]
+    ])
 
+    const completeTask = (id) => {
+        setTasks((prev) => prev.map((task) => (task.id === id ? { ...task, taskStatus: "Completed" } : task)));
+    }
+    const deleteTask = (id) => {
+        setTasks((prev) => prev.filter((task) => task.id !== id))
+    }
 
     return (
         <View>
