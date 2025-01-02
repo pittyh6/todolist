c3cdfd1fde465ce9a31ac04c414ece12ea02c3ed
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2025-01-02 16:46:55 +1100

    start swip btn

--- toDoList-reactNative/src/components/Tasks.js
+++ toDoList-reactNative/src/components/Tasks.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import {
+    StyleSheet, Text, View, SafeAreaView, Animated,
+    PanResponder, TouchableOpacity, FlatList,
+} from 'react-native';
 import { Swipeable } from "react-native-gesture-handler";
 
 
 
 function Tasks(props) {
     const tasks = [
-        { taskDescription: "Study", taskStatus: "Pending" },
-        { taskDescription: "Rest", taskStatus: "Completed" }
+        { id: 1, taskDescription: "React Native", taskStatus: "Pending" },
+        { id: 2, taskDescription: "Read", taskStatus: "Completed" },
+        { id: 3, taskDescription: "Draw", taskStatus: "Completed" },
+        { id: 4, taskDescription: "Rest", taskStatus: "Completed" },
     ]
+
+    const renderItem = ({ item }) => {
+        const translateX = useRef(new Animation.Value(0)).current;
+    }
 
     return (
         <View>
